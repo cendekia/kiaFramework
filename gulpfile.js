7ec1f3f4b7f4c506bfc5d5b075cbf7ca7996234c
cendekiapp <cendiqkrn@gmail.com>
2015-06-15 16:09:14 +0700

    generate all styles for admin

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,17 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.less('app.less');
+    mix.styles([
+    	'../admin/plugins/bootstrap/css/bootstrap.min.css',
+    	'../admin/css/libs/font-awesome.min.css',
+    	'../admin/css/libs/ionicons.min.css',
+    	'../admin/css/AdminLTE.min.css',
+    	'../admin/css/skins/_all-skins.min.css',
+    	'../admin/plugins/iCheck/flat/blue.css',
+    	'../admin/plugins/morris/morris.css',
+    	'../admin/plugins/jvectormap/jquery-jvectormap-1.2.2.css',
+    	'../admin/plugins/datepicker/datepicker3.css',
+    	'../admin/plugins/daterangepicker/daterangepicker-bs3.css',
+    	'../admin/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css',
+    ], 'public/admin/css');
 });
